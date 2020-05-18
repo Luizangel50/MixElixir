0adb2f662fff93610ae49f47afe9df721a8ca0dc
Luiz Angel <luiz.angel.100@gmail.com>
2020-05-18 17:28:41 -0300

    Setting up GenServer client API and server callbacks

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,19 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    bucket = start_supervised!(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    # `bucket` is now the bucket from the setup block
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+
+    KV.Bucket.put(bucket, "eggs", 4)
+    assert KV.Bucket.delete(bucket, "eggs") == 4
+  end
+end
